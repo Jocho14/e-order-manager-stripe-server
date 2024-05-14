560b631ecce100b9cf8f29053aa05e6f1c7825cf
Jocho14 <jochotyler1@gmail.com>
2024-05-14 17:38:22 +0200

    fix

--- src/server.ts
+++ src/server.ts
@@ -3,31 +3,26 @@ import { NextFunction } from "express";
 const express = require("express");
 const app = express();
 const cors = require("cors");
+app.use(express.static("public"));
+
+app.use((req: any, res: any, next: any) => {
+  res.header("Access-Control-Allow-Origin", "*"); // allows requests from all origins
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
 const {
   createCheckoutSession,
   getSessionStatus,
   getTestLog,
 } = require("./session");
-
-// app.use(cors({ origin: "*" }));
-app.use(express.static("public"));
-
-//app.use(cors({ origin: ["http://localhost:5173", "http://127.0.0.1:5173"] }));
-app.use(function (req: any, res: any, next: any) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
 
 app.post("/create-checkout-session", createCheckoutSession);
 app.get("/session-status", getSessionStatus);
