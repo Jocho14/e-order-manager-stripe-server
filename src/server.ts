31c4649da0c72e8cb9389c60bf7f60fab5bea237
Jocho14 <jochotyler1@gmail.com>
2024-05-14 17:30:18 +0200

    fix

--- src/server.ts
+++ src/server.ts
@@ -9,10 +9,25 @@ const {
   getTestLog,
 } = require("./session");
 
-app.use(cors({ origin: "*" }));
+// app.use(cors({ origin: "*" }));
 app.use(express.static("public"));
 
 //app.use(cors({ origin: ["http://localhost:5173", "http://127.0.0.1:5173"] }));
+app.use(function (req: any, res: any, next: any) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 
 app.post("/create-checkout-session", createCheckoutSession);
 app.get("/session-status", getSessionStatus);
