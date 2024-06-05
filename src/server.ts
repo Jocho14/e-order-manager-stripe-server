0d8f6ba5b25671ad4242ddc4540efe85dbb15fdf
Jocho14 <jochotyler1@gmail.com>
2024-06-05 12:27:40 +0200

    production port change

--- src/server.ts
+++ src/server.ts
@@ -16,5 +16,5 @@ app.post("/create-checkout-session", createCheckoutSession);
 app.get("/session-status", getSessionStatus);
 app.get("/test-log", getTestLog);
 
-const PORT = 4242;
+const PORT = 8080;
 app.listen(PORT, () => console.log(`Running on port ${PORT}`));
